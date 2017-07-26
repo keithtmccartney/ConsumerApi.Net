af8ef1d7b21d5f105e0d2bbbae763149ecab2ed0
Keith <keithtmccartney@hotmail.com>
2017-07-26 21:49:15 +0100

    Adding Transaction Model.

---
+++ ConsumerApi.Net/ConsumerApi.Net/Models/Transaction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsumerApi.Net.Models
+{
+    public class Transaction
+    {
+        public int TransactionId
+        {
+            get; set;
+        }
+
+        public DateTime TransactionDate
+        {
+            get; set;
+        }
+
+        public string Description
+        {
+            get; set;
+        }
+
+        public int TransactionAmount
+        {
+            get; set;
+        }
+
+        public DateTime CreatedDate
+        {
+            get; set;
+        }
+
+        public DateTime ModifiedDate
+        {
+            get; set;
+        }
+
+        public string CurrencyCode
+        {
+            get; set;
+        }
+
+        public string Merchant
+        {
+            get; set;
+        }
+    }
+}
