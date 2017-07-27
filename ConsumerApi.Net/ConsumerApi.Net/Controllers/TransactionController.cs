63684daf13c21decc187d12246685bfc18e62a98
Keith <keithtmccartney@hotmail.com>
2017-07-27 21:36:10 +0100

    Adding Controller changes.

--- ConsumerApi.Net/ConsumerApi.Net/Controllers/TransactionController.cs
+++ ConsumerApi.Net/ConsumerApi.Net/Controllers/TransactionController.cs
@@ -3,44 +3,95 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ConsumerApi.Net.Interfaces;
+using ConsumerApi.Net.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ConsumerApi.Net.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     public class TransactionController : Controller
     {
-        // GET: api/values
+        private readonly ITransactionRepository _transactionRepository;
+
+        public TransactionController(ITransactionRepository transactionRepository)
+        {
+            _transactionRepository = transactionRepository;
+        }
+
+        // GET: transaction
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Transaction> Get()
         {
-            return new string[] { "value1", "value2" };
+            return _transactionRepository.GetAll();
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // GET transaction/5
+        [HttpGet("{id}", Name = "GetTransaction")]
+        public IActionResult Get(int id)
         {
-            return "value";
+            var _transaction = _transactionRepository.GetById(id);
+
+            if (_transaction == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(_transaction);
         }
 
-        // POST api/values
+        // POST transaction
         [HttpPost]
-        public void Post([FromBody]string value)
+        public IActionResult Post([FromBody]Transaction _parameter)
         {
+            if (_parameter == null)
+            {
+                return BadRequest();
+            }
+
+            var _transaction = _transactionRepository.Add(_parameter);
+
+            return CreatedAtRoute("GetTransaction", new { id = _transaction.TransactionId }, _transaction);
         }
 
-        // PUT api/values/5
+        // PUT transaction/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public IActionResult Put(int id, [FromBody]Transaction _parameter)
         {
+            if (_parameter == null)
+            {
+                return BadRequest();
+            }
+
+            var _transaction = _transactionRepository.GetById(id);
+
+            if (_transaction == null)
+            {
+                return NotFound();
+            }
+
+            _parameter.TransactionId = id;
+
+            _transactionRepository.Update(_parameter);
+
+            return NoContent();
         }
 
-        // DELETE api/values/5
+        // DELETE transaction/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            var _transaction = _transactionRepository.GetById(id);
+
+            if (_transaction == null)
+            {
+                return NotFound();
+            }
+
+            _transactionRepository.Delete(_transaction);
+
+            return NoContent();
         }
     }
 }
